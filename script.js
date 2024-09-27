f5ebab9b2c82f4887d514d0c1a7b90418c715116
leila Torres <joansioux@gmail.com>
2024-09-27 16:10:34 +0200

    ajout a propos

--- script.js
+++ script.js
@@ -35,3 +35,32 @@ burgerButton.addEventListener('click', function() {
         closeIcon.style.display = 'none';
     }
 });
+
+const videoPlayer = document.getElementById('video-player');
+
+
+// Liste des vidéos et leurs titres
+const videos = [
+    { src:'videos/Vanessa-Vandy-beach-35022-filmsupply.mp4'},
+    { src: 'videos/Alex-Fuller-coastal-aerials-29127-filmsupply.mp4' },
+];
+
+let currentVideoIndex = 0;
+
+// Fonction pour charger et jouer la vidéo suivante
+function playNextVideo() {
+    if (currentVideoIndex >= videos.length) {
+        currentVideoIndex = 0; // Recommence à la première vidéo si la liste est finie
+    }
+    
+    const video = videos[currentVideoIndex];
+    videoPlayer.src = video.src;
+    videoPlayer.play();
+    currentVideoIndex++;
+}
+
+// Lecture de la première vidéo au chargement
+playNextVideo();
+
+// Passer à la vidéo suivante lorsqu'une vidéo se termine
+videoPlayer.addEventListener('ended', playNextVideo);
